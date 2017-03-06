5fac8af4461e2d02d308131ea8bc6f32a8a92234
Krzysztof Szczęsny <kaszczes@student.agh.edu.pl>
2017-03-06 09:36:00 +0100

    Travis fix [5] + tex customization

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ case $yn in
 		sudo apt-get install -y octave liboctave-dev octave-image octave-io octave-statistics octave-quaternion
 
 		# latex
-		sudo apt-get install -y texlive-base texlive-binaries texlive-science texlive-lang-polish texlive-lang-european texlive-bibtex-extra texlive-fonts-recommended latexdiff texstudio
+		sudo apt-get install -y texlive-base texlive-latex-recommended texlive-latex-extra texlive-binaries texlive-science texlive-lang-polish texlive-bibtex-extra texlive-fonts-recommended latexdiff texstudio
 
 		# OpenCV dependencies
 		sudo apt-get install -y build-essential
--- travis-install.sh
+++ travis-install.sh
@@ -50,7 +50,7 @@ function install_opencv {
 
 case "$BUILD" in
 	tex)
-		sudo apt-get install -y texlive-base texlive-binaries texlive-science texlive-lang-polish texlive-bibtex-extra texlive-fonts-recommended
+		sudo apt-get install -y texlive-base texlive-latex-recommended texlive-latex-extra texlive-binaries texlive-science texlive-lang-polish texlive-bibtex-extra texlive-fonts-recommended 
 		;;
 
 	library)
