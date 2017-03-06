3248680cc6bf0eec03d461d849bd9c92d6f4cba8
Krzysztof Szczęsny <kaszczes@student.agh.edu.pl>
2017-03-06 10:30:59 +0100

    Travis fix [7]

--- tex/run.sh
+++ tex/run.sh
@@ -4,9 +4,9 @@ ARTICLE=Szczesny_Twardowski_MSc
 
 ./clean.sh
 
-pdflatex -halt-on-error -interaction errorstopmode ${ARTICLE} &&
+pdflatex -halt-on-error -interaction nonstopmode ${ARTICLE} &&
 bibtex ${ARTICLE} &&
-pdflatex -halt-on-error -interaction errorstopmode ${ARTICLE} &&
-pdflatex -halt-on-error -interaction errorstopmode ${ARTICLE}
+pdflatex -halt-on-error -interaction nonstopmode ${ARTICLE} &&
+pdflatex -halt-on-error -interaction nonstopmode ${ARTICLE}
 
 exit $?
